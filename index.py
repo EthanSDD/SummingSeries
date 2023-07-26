36cf44b8772022dfc9f7f9c89e5c4d3db4e59d9f
EthanSDD <135563855+EthanSDD@users.noreply.github.com>
2023-07-26 12:48:46 +1000

    Added menubar and theming

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from unittest import result
 
 root = Tk()
 root.title("Calculator")
@@ -23,11 +24,17 @@ def switch(): # Changing Operation
 def calculate(): # Calculation
 
     if operation == "arithmetic":
-        result = int(entry1.get()) + int(entry2.get())
-        calculation.config(text="Arithmetic series is: " + str(result))
+        total = 0
+        for i in range(int(entry3.get())):
+            if int(entry3.get()) >= 0:
+                calculation.config(text="Error, do not input 0")
+            total += (int(entry3.get()) / 2) * (2 * int(entry1.get())) + int(entry3.get()) - 1 * int(entry2.get())
+        calculation.config(text="Arithmetic series is: " + str(total))
     else:
-        result = int(entry1.get()) * int(entry2.get())
-        calculation.config(text="Geometric series is: " + str(result))
+        total = 0
+        for i in range(int(entry3.get())):
+            total += int(entry1.get()) ** int(entry2.get())
+        calculation.config(text="Geometric series is: " + str(total))
 
 # Create labels
 
@@ -39,6 +46,56 @@ labelB.place(x=10, y=55)
 
 labelC = Label(root, text="Num of Terms")
 labelC.place(x=10, y=95)
+
+# Theming
+
+theme = "light"
+
+def switchtheme():
+    global theme
+
+    if theme == "light":
+        darkmode()
+        theme = "dark"
+    else:
+        lightmode()
+        theme = "light"
+
+def darkmode(): # Dark Theme
+    root.config(bg="#1f1f1f")
+
+    for themed in root.winfo_children():
+        if isinstance(themed, Entry):
+            themed.config(bg="#2a2a2a", fg="#ffffff")
+        elif isinstance(themed, Button):
+            themed.config(bg="#363636", fg="#ffffff")
+        elif isinstance(themed, Radiobutton):
+            themed.config(bg="#1F1F1F", fg="#ffffff")
+        elif isinstance(themed, Label):
+            themed.config(bg="#1F1F1F", fg="#ffffff")
+    calculation.config(fg="#ffffff")
+
+def lightmode(): # Light Theme
+    root.config(bg="#f0f0f0")
+
+    for themed in root.winfo_children():
+        if isinstance(themed, Entry):
+            themed.config(bg="#ffffff", fg="#000000")
+        elif isinstance(themed, Button):
+            themed.config(bg="#eeeeee", fg="#000000")
+        elif isinstance(themed, Radiobutton):
+            themed.config(bg="#f0f0f0", fg="#000000")
+        elif isinstance(themed, Label):
+            themed.config(bg="#f0f0f0", fg="#000000")
+    calculation.config(fg="#000000")
+
+# Translator
+def translation():
+    "hi"
+
+# Font Sizer
+def fontsizer():
+    "hi"
 
 # Create Entry Widget Input Box
 
@@ -52,11 +109,11 @@ entry3 = Entry(root, width=10, font=("Helvetica", 18))
 entry3.place(x=130, y=90)
 
 # Create Buttons
-
-button1 = Radiobutton(root, text="Switch Operation", command=switch)
+var = IntVar()
+button1 = Radiobutton(root, text="Switch Operation", command=switch, variable = var, value = 1)
 button1.place(x=10, y=130)
 
-button2 = Radiobutton(root, text="Calculate", command=calculate)
+button2 = Radiobutton(root, text="Calculate", command=calculate, variable = var, value = 2)
 button2.place(x=70, y=130)
 
 calculation = Label(root, text="")
@@ -64,5 +121,28 @@ calculation.place(x=10, y=160)
 
 clear = Button(root, width=5, text="Clear", command=lambda: calculation.config(text=""))
 clear.place(x=10, y=190)
+
+# Create File toolbar
+
+menubar = Menu(root)
+root.config(menu=menubar)
+
+menufile = Menu(menubar, tearoff=0)
+menufont = Menu(menubar, tearoff=0)
+menutran = Menu(menubar, tearoff=0)
+
+menubar.add_cascade(label="File", menu=menufile)
+menubar.add_cascade(label="Change Font Size", menu=menufont)
+menubar.add_cascade(label="Translate", menu=menutran)
+
+menufile.add_command(label="Switch Theme", command=switchtheme)
+menufile.add_separator()
+menufile.add_command(label="Exit", command=root.destroy)
+
+menufont.add_command(label="1")
+menufont.add_command(label="2")
+
+menutran.add_command(label="English")
+menutran.add_command(label="French")
 
 root.mainloop()
